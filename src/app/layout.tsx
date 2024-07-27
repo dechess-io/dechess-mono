e6c9a5eadb5ad7fa1ead610280a571d192fd6a95
thienba <macm3pro@Thien-Ba.local>
2024-07-27 23:56:43 +0700

    init source

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Space_Grotesk } from 'next/font/google'
+
+import './globals.css'
+
+const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Your Site Title',
+    template: '%s | Your Site Title'
+  },
+  description: 'Your site description',
+  keywords: 'keyword1, keyword2, keyword3',
+  manifest: '/favicon/site.webmanifest',
+  twitter: {
+    card: 'summary_large_image'
+  }
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={spaceGrotesk.className}>{children}</body>
+    </html>
+  )
+}
