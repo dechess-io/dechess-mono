4d79327d3c869ad063e35de48351f508229849ff
thienba <macm3pro@Thien-Ba.local>
2024-08-24 00:45:51 +0700

    fix(about): remove animation & downsize background image

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -6,21 +6,14 @@ const About = () => {
     >
       <div className="container">
         <div className="relative z-[1] flex justify-center h-full flex-col">
-          <h1
-            data-aos="fade-right"
-            className="font-planet text-2xl md:text-3xl md:leading-[4.625rem]"
-          >
+          <h1 className="font-planet text-2xl md:text-3xl md:leading-[4.625rem]">
             DeChess, <br /> Where{' '}
             <span className="bg-gradient-to-r from-yellow-17 to-yellow-43 bg-clip-text text-transparent">
               Brilliant
             </span>{' '}
             <br /> <span className="text-yellow-17">Moves</span> Begin
           </h1>
-          <p
-            data-aos="fade-right"
-            data-aos-duration="1500"
-            className="lg:text-normal"
-          >
+          <p className="lg:text-normal">
             Command your pieces like a Pharaoh in a <br /> game where each move
             carries the weight of an empire.
           </p>
